c3b023d3e1c7241043815fd1b10dc3f543ce23a7
anya.shan <anya.shanaida@yahoo.com>
2023-08-05 15:44:14 +0300

    add App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+export function App() {
+  //   const dispatch = useDispatch();
+  //   const { isRefreshing } = useAuth();
+
+  //   useEffect(() => {
+  //     dispatch(refreshUser());
+  //   }, [dispatch]);
+
+  return <h1>Hello</h1>;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { BrowserRouter } from 'react-router-dom';
+
+import { App } from 'components/App';
+// import { store, persistor } from './redux/store';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +16,20 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+
+//       <PersistGate loading={null} persistor={persistor}>
+
+//         <BrowserRouter basename="event-planner">
+//           <App />
+//         </BrowserRouter>
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
